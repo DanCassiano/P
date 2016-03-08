508c5da0fc412b770513e19d02cdbd26c8faaf39
DanCassiano <jordan_gnr@hotmail.com>
2016-03-08 19:47:45 -0300

    Add config do banco

--- public/index.php
+++ public/index.php
@@ -1,15 +1,24 @@
 <?php
- 	use Symfony\Component\HttpFoundation\Response;
- 	use Symfony\Component\HttpFoundation\Request;
-	require_once '../vendor/autoload.php';  
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\HttpFoundation\Request;
+	require_once '../vendor/autoload.php';
 	
-	$app = new Silex\Application(); 
+	$app = new Silex\Application();
 	$app->register(new Silex\Provider\SessionServiceProvider());
-
+	$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+			'db.options' => array(
+					'driver'    => 'pdo_mysql',
+					'host'      => 'localhost',
+					'dbname'    => 'gumball',
+					'user'      => 'root',
+					'password'  => 'root',
+					'charset'   => 'utf8mb4',
+				),
+		));
 	$app['debug'] = true;
 	$app['dir'] = dirname(__DIR__);
 	$app['basePath'] = dirname(__DIR__);
-	$app['dir_repo'] = "C:/wamp/www/";
+	$app['dir_repo'] = "/var/www/";
 	
 	Request::enableHttpMethodParameterOverride();
 	$app->mount('/', new Core\Controller());
